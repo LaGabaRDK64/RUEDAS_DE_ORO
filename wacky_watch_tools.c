71d2d47ea3d626c317b2a5706935b19f64352877
LaGabaRDK64 <gabrielagosto10.2007@gmail.com>
2026-06-25 00:28:11 -0500

    Cambios en wacky watch tools, solo eso de momento

--- wacky_watch_tools.c
+++ wacky_watch_tools.c
@@ -7,11 +7,18 @@
 /*encabezados*/
 #include "wacky_watch_tools.h"
 
-void _watch_integers_numbers(){
-    
+void _watch_integers_numbers(int _buffer_size){
+
+    int _number_len = _buffer_size;
     char buffer[20];
-    
-    printf(" (%s) >>>>> ", _local_messages[1]);
+
+    do
+    {
+        printf(" (%s) >>>>> ", _local_messages[1]);
+        fgets(buffer, _number_len, stdin);
+        
+
+    } while (buffer < 0);
 
 
 }
--- wacky_watch_tools.h
+++ wacky_watch_tools.h
@@ -1,13 +1,13 @@
 #ifndef wacky_watch_tools
 #define wacky_watch_tools
 
-    char _local_messages[3][30] = { "Ingrese su opcion aqui", "|| ERROR ||" , "Ingrese su opcion de nuevo"};
+    char _local_messages[3][60] = { "Ingrese su opcion aqui", "|| ERROR ||" , "Ingrese nuevamente la opcion acorde al mensaje en pantalla"};
 
-    void _watch_integers_numbers();
-    int _watch_integers_numbers_into_range(int max, int min);
-    void _watch_doubles_numbers();
-    float _watch_doubles_numbers_into_range(float max, float min);
-    void _watch_string_ALL_NORMALCHARS();
-    void _watch_string_NOTNUMBERS();
+    void _watch_integers_numbers(int _buffer_size);
+    int _watch_integers_numbers_into_range(int _buffer_size, int max, int min);
+    void _watch_doubles_numbers(int _buffer_size);
+    float _watch_doubles_numbers_into_range(int _buffer_size, float max, float min);
+    void _watch_string_ALL_NORMALCHARS(int _buffer_size);
+    void _watch_string_NOTNUMBERS(int _buffer_size);
 
 #endif
