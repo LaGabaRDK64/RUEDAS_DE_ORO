a4c10967fa1ab68bcc40ab02b5924cc5b71172a4
LaGabaRDK64 <gabrielagosto10.2007@gmail.com>
2026-06-25 03:49:31 -0500

    Funcionalidades incompletas de car

--- cars_default.c
+++ cars_default.c
@@ -13,12 +13,224 @@ void register_vehicle(int pos){
 
     printf("\nOPCION SELECIONADA: Agregar Auto al inventario\n\n");
 
-    printf("||| Ingrese el ID del carro que desea ingresar (RECOMENDACION: Solo un numero pequeño) |||\n");
-    _is_car_default._ID_Only_Number = _watch_integers_numbers();
-    
-    printf("||| Ingrese la marca del carro que desea ingresar |||\n");
+    _is_car_default._ID_Only_Number = list_vehicle();
+    printf("||| Ingrese la marca del carro con ID %d |||\n", _is_car_default._ID_Only_Number);
     _watch_string_NOTNUMBERS(_is_car_default._brand);
-    printf("||| Ingrese la marca del carro que desea ingresar |||\n");
+    printf("||| Ingrese el modelo del carro con ID %d |||\n",  _is_car_default._ID_Only_Number);
+    _watch_string_ALL_NORMALCHARS(_is_car_default._model);
+    printf("||| Ingrese el año del carro con ID %d |||\n",  _is_car_default._ID_Only_Number);
+    _is_car_default._year = _watch_integers_numbers_into_range(2026,1900);
+    printf("||| Ingrese el precio del carro con ID %d |||\n",  _is_car_default._ID_Only_Number);
+    _is_car_default._price = _watch_doubles_numbers();
+    _is_car_default._is_sold_or_not = 0;
+
+    FILE *CARS_DATA;
+
+    CARS_DATA = fopen("DATA_BIN/cars_tab.dat", "ab");
+
+    if (CARS_DATA == NULL)
+    {
+        printf(" %s : No se pudo abrir el archivo ", _local_messages[1]);
+        return;
+    }
+
+    fwrite(&_is_car_default, sizeof(Cars_OG), 1, CARS_DATA);
+
+    fclose(CARS_DATA);
+
+    printf("\n| | | DATOS REGISTRADOS CORRECTAMENTE | | |\n");  
+    printf("Presione Enter para continuar");
+    getchar();
+
+}
+
+void find_vehicle(){
+
+    FILE *CARS_DATA;
+
+    Cars_OG Cars_list_default;
+
+    int ID_SEARCH;
+    int Found = 0;
+
+}
+
+int list_vehicle(){ 
+
+    FILE *CARS_DATA;
+    Cars_OG Cars_list_default;
+
+    int _is_zero = 0;
+
+    CARS_DATA = fopen("DATA_BIN/cars_tab.dat", "rb");
+
+    if (CARS_DATA == NULL)
+    {
+        return 1;
+    }
+    while (fread(&Cars_list_default, sizeof(Cars_OG), 1, CARS_DATA))
+    {
+        if (Cars_list_default._ID_Only_Number > _is_zero )
+        {
+            _is_zero = Cars_list_default._ID_Only_Number;
+        }
+        
+    }
+    
+    fclose(CARS_DATA);
+
+    return _is_zero + 1;
+
+}
+
+void find_vehicle()
+{
+
+    FILE *CARS_DATA;
+    Cars_OG vehicle;
+
+    int id_search;
+    int found = 0;
+
+    printf("\nIngrese ID del vehículo:\n");
+
+    id_search =
+    _watch_integers_numbers_into_range(999999,1);
+    CARS_DATA =
+    fopen("DATA_BIN/cars_tab.dat","rb");
+
+    if(CARS_DATA == NULL)
+    {
+        printf("Archivo inexistente\n");
+        return;
+    }
+
+    while(fread(&vehicle,sizeof(Cars_OG),1,CARS_DATA))
+    {
+        if(vehicle._ID_Only_Number == id_search)
+        {
+            printf("\nVehículo encontrado\n");
+            printf("Marca: %s\n",
+            vehicle._brand);
+            printf("Modelo: %s\n",
+            vehicle._model);
+            printf("Precio: %.2f\n",
+            vehicle._price);
+
+            found = 1;
+
+            break;
+
+        }
 
 
+    }
+
+    fclose(CARS_DATA);
+
+
+
+    if(found == 0)
+
+        printf("No existe ese vehículo\n");
+
+
+}
+
+void modyfing_car()
+{
+
+    FILE *CARS_DATA;
+    Cars_OG _is_car_default;
+
+    int id_modify;
+    int found = 0;
+
+    printf("\nIngrese ID del vehículo a modificar:\n");
+    id_modify = _watch_integers_numbers_into_range(999999,1);
+    CARS_DATA = fopen("DATA_BIN/cars_tab.dat","rb+");
+
+    if(CARS_DATA == NULL)
+    {
+        printf(" %s : Archivo no encontrado \n", _local_messages[1]);
+        return;
+    }
+
+    while(fread(&_is_car_default,sizeof(Cars_OG),1,CARS_DATA))
+    {
+        if(_is_car_default._ID_Only_Number == id_modify)
+        {
+            _is_car_default._ID_Only_Number = list_vehicle();
+            printf("||| Ingrese la marca del carro con ID %d |||\n", _is_car_default._ID_Only_Number);
+            _watch_string_NOTNUMBERS(_is_car_default._brand);
+            printf("||| Ingrese el modelo del carro con ID %d |||\n",  _is_car_default._ID_Only_Number);
+            _watch_string_ALL_NORMALCHARS(_is_car_default._model);
+            printf("||| Ingrese el año del carro con ID %d |||\n",  _is_car_default._ID_Only_Number);
+            _is_car_default._year = _watch_integers_numbers_into_range(2026,1900);
+            printf("||| Ingrese el precio del carro con ID %d |||\n",  _is_car_default._ID_Only_Number);
+            _is_car_default._price = _watch_doubles_numbers();
+            _is_car_default._is_sold_or_not = 0;
+
+            fseek(CARS_DATA,-sizeof(Cars_OG),SEEK_CUR);
+            fwrite(&_is_car_default,sizeof(Cars_OG),1,CARS_DATA);
+            found = 1;
+
+            break;
+
+        }
+
+    }
+    fclose(CARS_DATA);
+
+    if(found){
+        printf("Vehículo actualizado\n");
+    }else{
+        printf("Vehículo no encontrado\n");
+    }
+}
+
+void eliminate_car_list()
+{
+
+    FILE *old_file;
+    FILE *new_file;
+    Cars_OG vehicle;
+    int id_delete;
+    int deleted = 0;
+    printf("\nID del vehículo a eliminar:\n");
+    id_delete =_watch_integers_numbers_into_range(999999,1);
+    old_file = fopen("DATA_BIN/cars_tab.dat","rb");
+    new_file = fopen("DATA_BIN/temp.dat","wb");
+
+    if(old_file == NULL)
+    {
+        printf("No existe archivo\n");
+        return;
+    }
+
+    while(fread(&vehicle,sizeof(Cars_OG),1,old_file))
+    {
+        if(vehicle._ID_Only_Number == id_delete)
+        {
+            deleted = 1;
+            continue;
+        }
+
+        fwrite(&vehicle,sizeof(Cars_OG),1,new_file);
+    }
+
+    fclose(old_file);
+    fclose(new_file);
+
+    remove("DATA_BIN/cars_tab.dat");
+    rename("DATA_BIN/temp.dat","DATA_BIN/cars_tab.dat");
+
+    if(deleted){
+
+        printf("Vehículo eliminado correctamente\n");
+
+    }else{
+
+        printf("No se encontró vehículo\n");
+    }
 }
--- cars_default.h
+++ cars_default.h
@@ -4,17 +4,18 @@
 typedef struct Cars_OG
 {
     int _ID_Only_Number;
-    int _is_sold_or_not;
+    int _is_sold_or_not; /* 0 = NO VENDIDO | 1 = SI VENDIDO */
     char _brand[50];
     char _model[50];
     int _year;
-    char type[30];
-    float price;
+    int _kilometers;
+    char _type[30];
+    float _price;
     
 }Cars_OG;
 
 void register_vehicle(int pos);
-void list_vehicle();
+int list_vehicle();
 void find_vehicle();
 void modyfing_car();
 void eliminate_car_list();
