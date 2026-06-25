5df17027f04fcd8d3a4cfdb0982e7ac53f08c633
LaGabaRDK64 <gabrielagosto10.2007@gmail.com>
2026-06-25 00:58:45 -0500

    Algunos cambios y mas codigo agregado

--- wacky_watch_tools.c
+++ wacky_watch_tools.c
@@ -7,16 +7,46 @@
 /*encabezados*/
 #include "wacky_watch_tools.h"
 
-void _watch_integers_numbers(int _buffer_size){
+void _watch_integers_numbers(){
 
-    int _number_len = _buffer_size;
     char buffer[20];
+    int number;
+    char *ptr = NULL;
 
     do
     {
-        printf(" (%s) >>>>> ", _local_messages[1]);
-        fgets(buffer, _number_len, stdin);
         
+        printf(" (%s) >>>>> ", _local_messages[0]);
+        fgets(buffer, sizeof(buffer), stdin);
+
+        if (buffer[0] == '\0' || buffer[0] == '\n')
+        {
+            printf("%s : El programa no admite espacios en blanco\n", _local_messages[1]);
+            printf("%s", _local_messages[2]);
+
+            continue;
+        }
+
+        number = strtol(buffer, &ptr, 10);
+
+        if(ptr == NULL)
+        {
+            printf("%s : El programa no admite letras\n", _local_messages[1]);
+            printf("%s", _local_messages[2]);
+            continue;
+            
+        }else if (*ptr != '\0' || *ptr != '\0')
+        {
+            printf("%s : Recuerda que solo lee numeros enteros\n", _local_messages[1]);
+            printf("%s", _local_messages[2]);
+
+        }
+
+        
+
+
+        
+
 
     } while (buffer < 0);
 
